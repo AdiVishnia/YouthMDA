9e33813041594d650c7cdbe99b077a7b29053e31
Adi Vishnia <adi.vishnia@gmail.com>
2025-12-07 17:12:55 +0200

    Fix merge conflict in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,7 @@
 module.exports = function(api) {
-<<<<<<< HEAD
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['module:react-native-dotenv'],
-  };
-}; 
-=======
     api.cache(true);
     return {
       presets: ['babel-preset-expo'],
-      plugins: ['module:react-native-dotenv'],
-
-      
+      plugins: ['module:react-native-dotenv'],  
     };
   };
-
->>>>>>> 1e2385bdcf63c729de6c9789d1e0506b30da1669
